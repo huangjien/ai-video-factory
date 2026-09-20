f8a4c5b24667dd3be273c752166bbb8df327b26c
huangjien <huangjien@gmail.com>
2026-09-20 21:25:46 +0100

    chore(audio-assets): add barrel export for downstream consumers

---
+++ packages/audio-assets/src/index.ts
@@ -0,0 +1,10 @@
+export {
+  AssetResultSchema,
+  AudioAssetError,
+  BgmOptionsSchema,
+  SfxOptionsSchema,
+  type AssetResult,
+  type AudioAssetProvider,
+  type BgmOptions,
+  type SfxOptions,
+} from "./schemas.js";
